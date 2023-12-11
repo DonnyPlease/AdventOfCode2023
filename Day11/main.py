a207a6c21ace4a0588655089409113c8ca26c655
Samuel <102140045+DonnyPlease@users.noreply.github.com>
2023-12-11 13:27:14 +0100

    remove input

--- Day11/main.py
+++ Day11/main.py
@@ -6,7 +6,7 @@ def read_input():
     return lines
 
 def get_empty_rows(lines):
-    return [i for i, line in enumerate(lines) if line == '.'*len(line)]
+    return [i for i, line in enumerate(lines) if all([ch == '.' for ch in line])]
 
 def get_empty_cols(lines):
     empty = []
@@ -18,25 +18,28 @@ def get_empty_cols(lines):
                 break
         if em:
             empty.append(i)
+            
+    # Alternative (more consise) solution from hyper-neutrino:        
+    # empty = [i for i, col in enumerate(zip(*lines)) if all([ch == '.' for ch in col])]
     return empty
     
 def get_galaxies(lines):
     return [(i,j) for i in range(len(lines)) for j in range(len(lines[0])) if lines[i][j] == '#']
 
 def find_distances(lines, expansion):
-    galaxies = get_galaxies(lines)
+    gs = get_galaxies(lines)
     empty_rows = get_empty_rows(lines)
     empty_cols = get_empty_cols(lines)
     
     distances = []
-    for i in range(len(galaxies)):
-        for j in range(i+1,len(galaxies)):
-            d = abs(galaxies[i][0] - galaxies[j][0]) + abs(galaxies[i][1] - galaxies[j][1])
+    for i in range(len(gs)):
+        for j in range(i+1,len(gs)):
+            d = abs(gs[i][0] - gs[j][0]) + abs(gs[i][1] - gs[j][1])
             for k in empty_rows:
-                if galaxies[i][0] < k < galaxies[j][0] or galaxies[j][0] < k < galaxies[i][0]:
+                if gs[i][0] < k < gs[j][0] or gs[j][0] < k < gs[i][0]:
                     d += expansion-1
             for k in empty_cols:
-                if galaxies[i][1] < k < galaxies[j][1] or galaxies[j][1] < k < galaxies[i][1]:
+                if gs[i][1] < k < gs[j][1] or gs[j][1] < k < gs[i][1]:
                     d += expansion-1
             distances.append(d)
     return distances
